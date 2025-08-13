31f6fdce285965e00d2713dc2fa90c01844f1033
Kartik-Dumadiya <kartikdumadiya0111@gmail.com>
2025-08-13 15:32:37 +0530

    added finally

--- server.js
+++ server.js
@@ -76,9 +76,10 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 // -------------------------------------
 
 const PORT = 3000;
